727727fcfc346eb92fa8c3426c7cacfd37735e37
nikmand <nikmand@outlook.com>
2021-04-12 23:16:54 +0300

    remove unsued code

--- data_sources.py
+++ data_sources.py
@@ -34,32 +34,6 @@ if __name__ == "__main__":
 
     documents = preprocess_data_sources(directory)
 
-    # Send ingest data sources into es
+    # Ingest data sources into elasticsearch
     for document in documents:
-        print(document['type'])
         es.index(index=index_name, ignore=400, doc_type='docket', body=document)
-
-
-    # result = es.search(
-    #     index="myindex")
-    #
-    # print(result)
-
-    # es.indices.refresh(index_name)
-    # cnt = es.cat.count(index_name, params={"format": "json"})
-    #
-    # # es.indices.delete(index=index_name, ignore=[400, 404])
-    #
-    # search_param = {
-    #     "query": {
-    #         "match": {
-    #             "type": "BuildingOperation"
-    #         }
-    #     }
-    # }
-    # #
-    # response = es.search(index=index_name, body=search_param)
-    # all_documents = response['hits']['hits'] # list of results
-    #
-    # for document in all_documents:
-    #     print(document['_source']['createdAt'])
--- weather_data.py
+++ weather_data.py
@@ -7,34 +7,14 @@ from datetime import datetime
 if __name__ == "__main__":
 
     api_onecall_url = '{}?lat={}&lon={}&units=metric&appid={}'.format(weather_onecall_url, athens_lat, athens_lon,
-                                                                   weather_api_key)
+                                                                      weather_api_key)
     response = requests.get(api_onecall_url).json()
 
-    # current = response['current']
     daily_forecasts = response['daily']
-    hourly_forecasts = response['hourly']
 
     daily_forecasts["dt"] = datetime.fromtimestamp(daily_forecasts["dt"])
 
     es = Elasticsearch(cloud_id=cluster_id, http_auth=("elastic", cluster_pwd))
 
-    # mapping = {
-    #     "mappings": {
-    #         "properties": {
-    #             "dt": {
-    #                 "type": "date",
-    #                 "format": "epoch_second"
-    #             }
-    #         }
-    #     }
-    # }
-
-    #response = es.indices.create(index=weather_index, body=mapping)
-
     for daily_forecast in daily_forecasts:
         es.index(index=weather_index_daily, doc_type='docket', body=daily_forecast)
-
-    for hourly_forecast in hourly_forecasts:
-        es.index(index=weather_index_hourly, doc_type='docket', body=hourly_forecast)
-
-
